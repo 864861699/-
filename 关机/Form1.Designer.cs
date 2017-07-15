2e76f04e04f580a87b6d352b472bad702b76d00d
aixiang <aixiang@qq.com>
2017-07-15 17:50:01 +0800

    1

---
+++ 关机/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace 关机
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(107, 29);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(43, 21);
+            this.tb1.TabIndex = 0;
+            this.tb1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "关机时间：";
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(187, 29);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(43, 21);
+            this.tb2.TabIndex = 3;
+            this.tb2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb2_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(161, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "：";
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(43, 77);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(75, 23);
+            this.bt1.TabIndex = 8;
+            this.bt1.Text = "关机";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "取消关机";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 127);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "关机";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button button2;
+
+
+    }
+}
+
---
+++ 关机/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 关机
+{
+    public partial class Form1 : Form
+    {
+        Sunisoft.IrisSkin.SkinEngine se = null;
+        public Form1()
+        {
+            InitializeComponent();
+            se = new Sunisoft.IrisSkin.SkinEngine();
+            se.SkinAllForm = true;//所有窗体均应用此皮肤
+            se.SkinFile = "skin/MacOS.ssk";
+        }
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            if (tb1.Text == "" || tb2.Text == "" || tb1.Text == null || tb2.Text==null)
+            {
+                MessageBox.Show("小时分钟都要写");
+                return;
+            }
+            Process myProcess = new System.Diagnostics.Process();
+            myProcess.StartInfo.FileName = "cmd.exe";
+            myProcess.StartInfo.UseShellExecute = false;
+            myProcess.StartInfo.RedirectStandardInput = true;
+            myProcess.StartInfo.RedirectStandardOutput = true;
+            myProcess.StartInfo.RedirectStandardError = true;
+            myProcess.StartInfo.CreateNoWindow = true;
+            myProcess.Start();
+            int a = int.Parse(tb1.Text);
+            int b = int.Parse(tb2.Text);
+            if (a > 23)
+            {
+                MessageBox.Show("小时不要写这么大好不好<(－︿－)>");
+            }
+            else if (b > 59)
+            {
+                MessageBox.Show("分钟不要写这么大好不好￣へ￣");
+            }
+            else
+            {
+                string time = a + ":" + b;
+
+                //获取当前时间和结束时间
+                string daystr = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                string dt1 = daystr.Split(new char[] { ' ' })[0];
+                string endtime = daystr.Split(new char[] { ' ' })[0] + " " + time;
+
+                //算出毫秒
+                DateTime t1 = Convert.ToDateTime(endtime);
+                DateTime t2 = Convert.ToDateTime(daystr);
+                TimeSpan t3 = t1 - t2;
+                double getSeconds = t3.TotalSeconds;
+
+                if (getSeconds > 0)
+                {
+                    myProcess.StandardInput.WriteLine("shutdown -s -t " + getSeconds.ToString() + "");
+                }
+                else
+                {
+                    //重新获取结束时间
+                    string[] arr = daystr.Split(new char[] { ' ' })[0].Split(new char[] { '-' });
+                    string str = arr[0] + "-" + arr[1] + "-" + (int.Parse(arr[2]) + 1).ToString();
+                    endtime = str + " " + time;
+                    t1 = Convert.ToDateTime(endtime);
+                    t3 = t1 - t2;
+                    getSeconds = t3.TotalSeconds;
+                    myProcess.StandardInput.WriteLine("shutdown -s -t " + getSeconds.ToString() + "");
+                }
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process myProcess = new System.Diagnostics.Process();
+            myProcess.StartInfo.FileName = "cmd.exe";
+            myProcess.StartInfo.UseShellExecute = false;
+            myProcess.StartInfo.RedirectStandardInput = true;
+            myProcess.StartInfo.RedirectStandardOutput = true;
+            myProcess.StartInfo.RedirectStandardError = true;
+            myProcess.StartInfo.CreateNoWindow = true;
+            myProcess.Start();
+            myProcess.StandardInput.WriteLine("shutdown -a"); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strDt = DateTime.Now.ToString("t");
+            string[] dt1 = strDt.Split(new char[] { ':' });
+            int time = int.Parse(dt1[0]);
+            time += 1;
+            if (time==24)
+            {
+                time = 0;
+            }
+            
+            tb1.Text = time.ToString();
+            tb2.Text = dt1[1];
+        }
+
+        private void tb1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 允许输入:数字、退格键(8)、全选(1)、复制(3)、粘贴(22)
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8 &&
+            e.KeyChar != 1 && e.KeyChar != 3 && e.KeyChar != 22)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tb2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 允许输入:数字、退格键(8)、全选(1)、复制(3)、粘贴(22)
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8 &&
+            e.KeyChar != 1 && e.KeyChar != 3 && e.KeyChar != 22)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
